2b5cb7c314bdec37e6f4ce742eb2803f1afd39d1
Brent Scott <brentscott@mailfence.com>
2022-11-21 11:32:45 -0600

    update flake

--- flake.nix
+++ flake.nix
@@ -6,14 +6,25 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      #rstudio = pkgs.rstudioWrapper.override{ packages = with pkgs.rPackages; [pagedown]; };
+      rmark = pkgs.rPackages.buildRPackage {
+        name = "rmarkdown";
+        src = pkgs.fetchFromGitHub{
+          owner = "rstudio";
+          repo = "rmarkdown";
+          rev = "b87ca50c8c4d5a5876333b598aed4eb84de925a3";
+          sha256 = "12mhmmibizbxgmsns80c8h97rr7rclv9hz98zpgsl26hw3s4l0vm";
+        };
+      };
     in {
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
         buildInputs = with pkgs; [ R
                                    rPackages.pagedown
+                                   rmark
                                    chromium
                                    pandoc
-                                   rstudioWrapper.override{ packages = with rPackages; [pagedown] }
+				   rstudio
                                  ];
       };
     });
